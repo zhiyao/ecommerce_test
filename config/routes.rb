dc3eed33f8aeee59dc9a8fc600299194947dca44
Kenny Chan Zhi Yao <zhiyao.chan@gmail.com>
2016-07-07 17:02:21 +0800

    added scaffolding for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'products#index'
+  root 'home#index'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  resources :categories
 
   namespace :admin do
     root 'products#index'
